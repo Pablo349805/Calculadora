b04d8cba8cd72aba86aa933720fa1fd4bca79723
Pablo349805 <4984519gmail.com>
2023-04-03 09:37:05 -0300

    agregamos

---
+++ CalcApp/script.js
@@ -0,0 +1,19 @@
+let edad = prompt("que edad tiene?");
+
+if (edad>=18) {
+    alert("bienvenido/a al bar!")
+    
+let numeroSecreto = 10
+let loQueDiceLaPersona = prompt("cual es el numero secreto?")
+
+if (numeroSecreto == loQueDiceLaPersona) {
+    alert("puede pasar a la fiesta secreta .....pero SSHHHHHH!")
+}else{
+    alert("Ese no es el numero secreto! No puede ingresar")
+}
+}else{
+    alert("No puede pasar! lo siento...")
+}
+
+
+
